9756edac9312d9d2109f2604e7e516f6011b0535
Eric Bastos Gorgens <2597395+Gorgens@users.noreply.github.com>
2023-01-18 15:42:30 -0300

    update code

--- flower mask.R
+++ flower mask.R
@@ -1,53 +1,100 @@
 require(raster)
 require(tmap)
+require(dplyr)
 
-site = stack('area_experimento.tif')
-site
+vant = stack('./antes/orto_medio_4marcador_corrigido.tif')
+tratamentos = shapefile('./antes/Tratamento_remocao.shp')
 
-grayscale_colors <- gray.colors(100,            # number of different color levels 
-                                start = 0.0,    # how black (0) to go
-                                end = 1.0,      # how white (1) to go
-                                gamma = 2.2,    # correction between how a digital 
-                                # camera sees the world and how human eyes see it
-                                alpha = NULL)   #Null=colors are not transparent
+tratamentoEscolhido = subset(tratamentos, rotulo =="L1C1")
+vantTratamentoEscolhido = crop(vant, tratamentoEscolhido)
 
-plot(site@layers[[1]], col=grayscale_colors)
-plot(site@layers[[2]], col=grayscale_colors)
-plot(site@layers[[3]], col=grayscale_colors)
+plotRGB(vantTratamentoEscolhido)
 
-siteBrick = brick(site@layers[[1]], site@layers[[2]], site@layers[[3]])
-plotRGB(siteBrick)
+# Baseado no valor numérico do pixel -------------------
+refPixelMin = 220
+refPixelMax = 230
+banda1 = vantTratamentoEscolhido[[1]]
+banda1[!((banda1 > refPixelMin) & (banda1 < refPixelMax))] = 0
+banda1[(banda1 > refPixelMin) & (banda1 < refPixelMax)] = 1
 
-# Teste baseado no valor numérico do pixel
-refPixel = 130
-a = site@layers[[1]]
-a[a < refPixel] = 0
-a[a >= refPixel] = 1
+refPixelMin = 220
+refPixelMax = 230
+banda2 = vantTratamentoEscolhido[[2]]
+banda2[!((banda2 > refPixelMin) & (banda2 < refPixelMax))] = 0
+banda2[(banda2 > refPixelMin) & (banda2 < refPixelMax)] = 1
 
-b = site@layers[[2]]
-b[b < refPixel] = 0
-b[b >= refPixel] = 1
+refPixelMin = 220
+refPixelMax = 230
+banda3 = vantTratamentoEscolhido[[3]]
+banda3[!((banda3 > refPixelMin) & (banda3 < refPixelMax))] = 0
+banda3[(banda3 > refPixelMin) & (banda3 < refPixelMax)] = 1
 
-c = site@layers[[3]]
-c[c < refPixel] = 0
-c[c >= refPixel] = 1
+pixelFlorais = (banda1 + banda2 + banda3)
+pixelFlorais[pixelFlorais == 3] = 1
+pixelFlorais[!(pixelFlorais == 1)] = 0
 
-featureOfInterest = ceiling((a + b + c)/3)
+pixelFlorais.df = as.data.frame(pixelFlorais) 
+sum(pixelFlorais.df$layer)
+
 tm_shape(featureOfInterest) + 
   tm_raster()
 
-# teste da classificação não supervisionada
-image.df = as.data.frame(site)  
-cluster.image = kmeans(na.omit(image.df), centers=15)
+# Classificação não supervisionada ------------------
+vantTratamentoEscolhido.df = as.data.frame(vantTratamentoEscolhido)  
+ajusteKmeans = kmeans(na.omit(vantTratamentoEscolhido.df), centers=15, algorithm="MacQueen", iter.max=1000)
 
-image.df.factor = rep(NA, length(image.df[,1]))
-image.df.factor[!is.na(image.df[,1])] = cluster.image$cluster
+ajusteKmeansFactor = rep(NA, length(vantTratamentoEscolhido.df[,1]))
+ajusteKmeansFactor[!is.na(vantTratamentoEscolhido.df[,1])] = ajusteKmeans$cluster
 
-clusters = raster(site)   ## create an empty raster with same extent than "image"  
-clusters = setValues(clusters, image.df.factor) ## fill the empty raster with the class results  
-
-tm_shape(clusters) + 
+imagemKmeans = raster(vantTratamentoEscolhido)   ## create an empty raster with same extent than "image"  
+imagemKmeans = setValues(imagemKmeans, ajusteKmeansFactor) ## fill the empty raster with the class results  
+tm_shape(imagemKmeans) + 
   tm_raster()
 
-tm_shape(a) + 
+pixelFlorais = imagemKmeans
+class = 15
+pixelFlorais[!(pixelFlorais == class)] = 0
+pixelFlorais[pixelFlorais == class] = 1
+
+pixelFlorais.df = as.data.frame(pixelFlorais) 
+sum(pixelFlorais.df$layer)
+
+tm_shape(pixelFlorais) + 
   tm_raster()
+
+# Classificação supervisionada ---------------------
+pontosTreinamento = shapefile('./antes/pontosTreinamento.shp')
+
+names(vant) = c("red", "green", 'blue', 'alpha')
+vant = vant[[-4]]
+
+sampvals = extract(vant, pontosTreinamento, df = TRUE)
+sampvals = sampvals[, -1]
+sampdata = data.frame(classvalue = pontosTreinamento@data$class, sampvals)
+
+mediaGrupo = sampdata %>%
+  group_by(classvalue) %>%
+  summarise(red = mean(red),
+            green = mean(green),
+            blue = mean(blue))
+
+ggplot(sampdata, aes(x = red, y = green)) + 
+  geom_point(aes(colour = factor(classvalue))) + 
+  geom_point(data = mediaGrupo, aes(red, green), shape = 4, size = 3) +
+  scale_color_manual(values = c("1" = "green", "2" = "gray"))
+
+ggplot(sampdata, aes(x = green, y = blue)) + 
+  geom_point(aes(colour = factor(classvalue))) + 
+  geom_point(data = mediaGrupo, aes(green, blue), shape = 4, size = 3) +
+  scale_color_manual(values = c("1" = "green", "2" = "gray"))
+
+ggplot(sampdata, aes(x = red, y = blue)) + 
+  geom_point(aes(colour = factor(classvalue))) + 
+  geom_point(data = mediaGrupo, aes(red, blue), shape = 4, size = 3) +
+  scale_color_manual(values = c("1" = "green", "2" = "gray"))
+
+
+mylogit = glm(classvalue ~ red + green + blue, data = sampdata, family = "binomial")
+imaClass = predict(vant, mylogit)
+mycolor <- c("gray", "green")
+plot(imaClass, main = 'Supervised classification - Logistica', col=mycolor)
